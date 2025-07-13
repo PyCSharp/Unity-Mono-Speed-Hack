18aae692993d55853e25858e0ade1c8c7b0dbd41
PyCSharp <PyCSharp@protonmail.com>
2025-07-13 21:38:48 +0200

    Add files via upload

---
+++ Cheat DLL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace CheatDLL
+{
+    public class Cheat : MonoBehaviour
+    {
+        private Rect windowRect = new Rect(100, 100, 300, 75);
+        public float hSliderValue = 1.0F;
+        public bool speedChanged;
+
+        void OnGUI()
+        {
+            windowRect = GUI.Window(0, windowRect, DrawWindow, "Speed Hack");
+        }
+
+        void DrawWindow(int windowID)
+        {
+            GUI.Label(new Rect(140, 25, 100, 20), Mathf.Round(hSliderValue).ToString());
+            hSliderValue = GUI.HorizontalSlider(new Rect(50, 40, 200, 100), hSliderValue, 0.0F, 100.0F);
+
+
+            GUI.DragWindow(new Rect(0, 0, 10000, 20));
+        }
+
+        public void Update()
+        {
+            Time.timeScale = hSliderValue;
+        }
+    }
+
+    public class Loader
+    {
+        public static void Init()
+        {
+            GameObject gameObject = new GameObject("CheatObject");
+            gameObject.AddComponent<Cheat>();
+        }
+    }
+}
---
+++ Speed Hack Loader.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Speed_Hack_Loader
+{
+    public partial class SpeedHack : Form
+    {
+        public string tempPath;
+        public string smiPath;
+        public string cheatPath;
+
+        public SpeedHack()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tempPath = Path.GetTempPath();
+
+            smiPath = Path.Combine(tempPath, "smi.exe");
+
+            cheatPath = Path.Combine(tempPath, "CheatDLL.dll");
+
+            var assembly = Assembly.GetExecutingAssembly();
+
+            ExtractBinaryResource(assembly, "Speed_Hack_Loader.CheatDLL.dll", Path.Combine(tempPath, "CheatDLL.dll"));
+
+            ExtractBinaryResource(assembly, "Speed_Hack_Loader.SharpMonoInjector.dll", Path.Combine(tempPath, "SharpMonoInjector.dll"));
+
+            ExtractBinaryResource(assembly, "Speed_Hack_Loader.smi.exe", Path.Combine(tempPath, "smi.exe"));
+
+            Process[] processes = Process.GetProcesses();
+
+            foreach (Process process in processes)
+            {
+                string processName = process.ProcessName;
+                GameDropDown.Items.Add(processName);
+            }
+        }
+
+        private void GameDropDown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GameLabel_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            Process.Start(smiPath, $"inject -p \"{GameDropDown.SelectedItem.ToString()}\" -a \"{cheatPath}\" -n CheatDLL -c Loader -m Init");
+        }
+
+        static void ExtractBinaryResource(Assembly assembly, string resourceName, string outputPath)
+        {
+            using (Stream resourceStream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (resourceStream == null)
+                {
+                    MessageBox.Show($"Resource {resourceName} not found!");
+                    return;
+                }
+
+                using (FileStream fileStream = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+                {
+                    resourceStream.CopyTo(fileStream);
+                }
+            }
+        }
+    }
+}
